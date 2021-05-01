b1ace59f5eca4fbe923ffae33526bb6877593ce6
fahmi54321 <fahmiabdulaziz44@gmail.com>
2021-05-01 21:08:21 +0700

    play youtube

--- app/src/main/java/com/android/a11playyoutube/MainActivity.java
+++ app/src/main/java/com/android/a11playyoutube/MainActivity.java
@@ -3,6 +3,8 @@ package com.android.a11playyoutube;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.loadUrl("https://www.youtube.com/watch?v=1vkJQ69_MTE");
+
     }
 }
